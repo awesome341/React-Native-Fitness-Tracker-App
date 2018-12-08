efc0290a32cd460a7159dcb39669284bf7534452
Excellent1212 <frankgass89@gmail.com>
2018-12-08 15:50:54 -0200

    feat: Increment, Decrement and slide logic

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,43 @@ import { View } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
 
 export default class AddEntry extends Component {
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0
+    }
+
+    increment = (metric) => {
+        const { max, step } = getMetricMetaInfo(metric)
+        this.setState( (state) => {
+            const count = state[metric] + step
+            
+            return {
+                ...state,
+                [metric]: count > max ? max : count
+            }
+        })
+    }
+
+    slide = (metric, value) => {
+        this.setState(() => ({
+            [metric]: value
+        }))
+    }
+
+    decrement = (metric) => {
+        this.setState((state) => {
+            const count = state[metric] - getMetricMetaInfo(metric).step
+
+            return {
+                ...state,
+                [metric]: count < 0 ? 0 : count
+            }
+        })
+    }
+    
     render = () => (
         <View>
             {getMetricMetaInfo('bike').getIcon()}
