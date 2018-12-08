1464ca2d370d1ff9cba1e699c0c73e816054fb33
Excellent1212 <frankgass89@gmail.com>
2018-12-08 17:23:41 -0200

    feat: Stepper component

--- components/UdaciStepper.js
+++ components/UdaciStepper.js
@@ -1,9 +1,29 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { 
+    View, 
+    Text,
+    TouchableOpacity,
+} from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
-const UdaciStepper = () => (
+const UdaciStepper = ({max, unit, step, value, onIncrement, onDecrement}) => (
     <View>
-        <Text>UdaciStepper</Text>
+        <View>
+            <TouchableOpacity
+                onPress={onDecrement}
+            >
+                <Ionicons name='ios-remove' size={30} />
+            </TouchableOpacity>
+            <TouchableOpacity
+                onPress={onIncrement}
+            >
+                <Ionicons name='ios-add' size={30} />
+            </TouchableOpacity>
+        </View>
+        <View>
+            <Text>{value}</Text>
+            <Text>{unit}</Text>
+        </View>
     </View>
 )
 
